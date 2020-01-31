c0fcfa43f67bb2a108f54d4c4b0dcd3f4e6a3c7d
Junjie Yang <junjie.yang2@mail.mcgill.ca>
2020-01-31 12:49:04 -0500

    Adding Gradle wrapper

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Registration.java
@@ -0,0 +1,43 @@
+package ca.mcgill.ecse321.eventregistration.model;
+import Event;
+import Person;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Id;
+
+@Entity
+public class Registration {
+	private Person person;
+
+	@ManyToOne(optional = false)
+	public Person getPerson() {
+		return this.person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	private Event event;
+
+	@ManyToOne(optional = false)
+	public Event getEvent() {
+		return this.event;
+	}
+
+	public void setEvent(Event event) {
+		this.event = event;
+	}
+
+	private Integer id;
+
+	public void setId(Integer value) {
+		this.id = value;
+	}
+
+	@Id
+	public Integer getId() {
+		return this.id;
+	}
+}
---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/RegistrationManager.java
@@ -0,0 +1,48 @@
+package ca.mcgill.ecse321.eventregistration.model;
+
+@Entity
+public class RegistrationManager {
+	private Set<Person> person;
+
+	@OneToMany(cascade = { CascadeType.ALL })
+	public Set<Person> getPerson() {
+		return this.person;
+	}
+
+	public void setPerson(Set<Person> persons) {
+		this.person = persons;
+	}
+
+	private Set<Registration> registration;
+
+	@OneToMany(cascade = { CascadeType.ALL })
+	public Set<Registration> getRegistration() {
+		return this.registration;
+	}
+
+	public void setRegistration(Set<Registration> registrations) {
+		this.registration = registrations;
+	}
+
+	private Set<Event> event;
+
+	@OneToMany(cascade = { CascadeType.ALL })
+	public Set<Event> getEvent() {
+		return this.event;
+	}
+
+	public void setEvent(Set<Event> events) {
+		this.event = events;
+	}
+
+	private Integer id;
+
+	public void setId(Integer value) {
+		this.id = value;
+	}
+
+	@Id
+	public Integer getId() {
+		return this.id;
+	}
+}
